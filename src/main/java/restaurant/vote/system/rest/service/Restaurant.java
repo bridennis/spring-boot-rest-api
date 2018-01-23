baed12e0ccc927c857c4b83e4273113b8eee481e
bridennis <5626164+bridennis@users.noreply.github.com>
2018-01-23 19:21:55 +0300

    initial of Restaurant service

---
+++ src/main/java/restaurant/vote/system/rest/service/Restaurant.java
@@ -0,0 +1,8 @@
+package restaurant.vote.system.rest.service;
+/*
+    Сюда необходимо перенести всю бизнес логику из RestaurantRestController,
+    в особенности из метода updateVote
+ */
+public class Restaurant {
+
+}
